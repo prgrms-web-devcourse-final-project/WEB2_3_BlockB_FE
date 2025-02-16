21a6e769d254c731f5662ca9af2a5015d57c1465
engsal <imaria0218@gmail.com>
2025-02-16 15:36:42 +0900

    docs:체크리스트-상수파일 생성 및 progress이미지 import 객체 파일 생성+글꼴 weight수정

---
+++ src/constants/index.ts
@@ -0,0 +1,38 @@
+export const continentChecklist = [
+  { key: "아프리카", isChecked: false },
+  { key: "남미", isChecked: false },
+  { key: "아시아", isChecked: false },
+  { key: "유럽", isChecked: false },
+  { key: "오세아니아", isChecked: false },
+];
+export const categoryChecklist = [
+  { key: "정치", isChecked: false },
+  { key: "경제", isChecked: false },
+  { key: "사회", isChecked: false },
+  { key: "문화/생활", isChecked: false },
+  { key: "연예", isChecked: false },
+  { key: "IT/과학", isChecked: false },
+  { key: "칼럼", isChecked: false },
+];
+export const participantChecklist = [
+  { key: "1:1", isChecked: false },
+  { key: "3:3", isChecked: false },
+];
+
+export const stanceChecklist = [
+  { key: "찬성", isChecked: false },
+  { key: "반대", isChecked: false },
+];
+export const hasVoteChecklist = [
+  { key: "승패 결정", isChecked: false },
+  { key: "승패 없음", isChecked: false },
+];
+export const timeChecklist = [
+  { key: "5분", isChecked: false },
+  { key: "10분", isChecked: false },
+  { key: "20분", isChecked: false },
+  { key: "25분", isChecked: false },
+  { key: "30분", isChecked: false },
+  { key: "35분", isChecked: false },
+  { key: "40분", isChecked: false },
+];
