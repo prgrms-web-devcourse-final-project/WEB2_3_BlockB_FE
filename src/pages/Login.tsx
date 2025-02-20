381d1527a60484b1c222d018f74c2009df231255
naehyun <scott253@naver.com>
2025-02-20 11:32:45 +0900

    feat: 로그인 페이지 footer status "defaul"  추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,9 +5,9 @@ import naver from "../assets/icons/naver.svg";
 import Footer from "../components/common/Footer";
 export default function Login() {
   return (
-    <div className="h-screen flex flex-col justify-between bg-gray-50">
+    <div className="flex flex-col justify-between h-screen bg-gray-50">
       {/* 중앙 로그인 박스 */}
-      <div className="flex flex-1 items-center justify-center">
+      <div className="flex items-center justify-center flex-1">
         <div className="bg-gray-100 p-10 rounded-xl shadow-lg text-center w-[400px]">
           {/* 로고 */}
           <h1 className="text-[76px] font-bold font-unifrakturCook justify-center">
@@ -19,29 +19,29 @@ export default function Login() {
 
           {/* 로그인 버튼들 */}
           <div className="space-y-3 font-pretendard">
-            <button className="flex items-center w-full py-3 bg-white border border-gray-300 rounded-md shadow-sm text-gray-700 font-medium relative">
+            <button className="relative flex items-center w-full py-3 font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm">
               <img
                 src={google}
                 alt="Google Logo"
-                className="w-6 h-6 absolute left-4"
+                className="absolute w-6 h-6 left-4"
               />
               <span className="w-full text-center">구글 로그인 하기</span>
             </button>
 
-            <button className="flex items-center w-full py-3 bg-yellow-400 rounded-md text-black font-medium relative">
+            <button className="relative flex items-center w-full py-3 font-medium text-black bg-yellow-400 rounded-md">
               <img
                 src={kakao}
                 alt="Kakao Logo"
-                className="w-6 h-6 absolute left-4"
+                className="absolute w-6 h-6 left-4"
               />
               <span className="w-full text-center">카카오 로그인 하기</span>
             </button>
 
-            <button className="flex items-center w-full py-3 bg-green-500 text-white rounded-md font-medium relative">
+            <button className="relative flex items-center w-full py-3 font-medium text-white bg-green-500 rounded-md">
               <img
                 src={naver}
                 alt="Naver Logo"
-                className="w-6 h-6 absolute left-4"
+                className="absolute w-6 h-6 left-4"
               />
               <span className="w-full text-center">네이버 로그인 하기</span>
             </button>
@@ -50,7 +50,7 @@ export default function Login() {
       </div>
 
       {/* 푸터 */}
-      <Footer />
+      <Footer status="default" />
     </div>
   );
 }
