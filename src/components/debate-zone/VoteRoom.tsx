02aa2dfd79706323feee8f171f42fcc26b9015e4
engsal-bin <imaria0218@gmail.com>
2025-02-23 13:50:38 +0900

    feat: 토론 투표 (voteroom) 반응형 구현 완료

--- src/components/debate-zone/VoteRoom.tsx
+++ src/components/debate-zone/VoteRoom.tsx
@@ -15,24 +15,24 @@ export default function VoteRoom({
     else setRoomState(stage);
   };
   const btnClass =
-    "w-[46px] h-[30px] px-[10px] py-[4px] bg-white text-black01 text-[14px] font-bold font-pretendard rounded-[5px] hover:bg-gray-300 hover:bg-game_blue01 hover:text-white transition-colors duration-300";
+    "md:w-[46px] w-[42px] md:h-[30px] h-[20px] md:px-[10px] px-2 md:py-[4px] bg-white text-black01 font-bold font-pretendard rounded-[5px] hover:bg-gray-300 hover:bg-game_blue01 hover:text-white transition-colors duration-300";
   return (
     <div className="flex flex-col justify-center items-center gap-[30px] min-h-screen">
       <div>
-        <h1 className="text-white font-pretendard font-bold text-[24px]">
+        <h1 className="text-white font-pretendard font-bold md:text-[24px] text-[18px]">
           토론이 끝났습니다. 승리했다고 생각하시는 입장에 투표해주세요
         </h1>
         <h2>AI는 인간의 노동을 대체하나 보조하나?</h2>
       </div>
       <div>
-        <section className="flex items-center gap-[26px]">
+        <section className="flex items-center md;gap-[26px] gap-1 md:text-[16px] text-[14px]">
           <div className="flex flex-col items-center gap-[26px]">
             <ParticipantBox label="PROS" labelAlignment="center" />
             <button onClick={() => moveState("result")} className={btnClass}>
               투표
             </button>
           </div>
-          <p className="text-white font-bold text-[24px] font-jersey">vs</p>
+          <p className="text-white font-bold md:text-[24px] text-[18px] font-jersey">vs</p>
           <div className="flex flex-col items-center gap-[26px]">
             <ParticipantBox label="CONS" color="blue" labelAlignment="center" />
             <button onClick={() => moveState("result")} className={btnClass}>
@@ -40,10 +40,10 @@ export default function VoteRoom({
             </button>
           </div>
         </section>
-        <div className="w-full flex justify-between mt-[60px]">
+        <div className="w-full flex justify-between mt-[60px] md:text-[16px] text-[14px]">
           <button
             onClick={() => moveState("replay")}
-            className="font-pretendard text-white border-b  "
+            className="font-pretendard text-white border-b"
           >
             토론 다시보기
           </button>
