ee3059e8136e3cdcc39be3ac4dc969f2c98fcf44
engsal-bin <imaria0218@gmail.com>
2025-02-23 14:46:01 +0900

    feat: 참관자 대기방 반응형 구현 완료

--- src/components/debate-zone/observer-mode/ObserverWaitingRoom.tsx
+++ src/components/debate-zone/observer-mode/ObserverWaitingRoom.tsx
@@ -4,6 +4,7 @@ import WaitingInfoDrodown from "./../waiting-room/WaitingInfoDrodown";
 import profile from "../../../assets/icons/profile-white.svg";
 import { useObservingStore } from "../../../stores/observingStateStore";
 import { useState } from "react";
+import AudienceListBox from "./AudienceListBox";
 
 export default function ObserverWaitingRoom() {
   const [isWaiting, setIsWaiting] = useState<boolean>(true);
@@ -11,26 +12,10 @@ export default function ObserverWaitingRoom() {
   if (isWaiting)
     return (
       <section className="px-[40px] flex flex-col gap-[250px] relative min-h-screen">
-        <div>
+        <div className="w-full flex justify-between">
           <WaitingInfoDrodown isWaiting={isWaiting} />
-          <div className=" w-full font-jersey text-white flex flex-col justify-normal items-end gap-[5px]">
-            <div className="w-[188px] flex justify-start">
-              <p>audience</p>
-            </div>
-            <figure className="flex gap-[11px] px-[5px] py-[2px] items-center  bg-white bg-opacity-40 w-[188px] h-[33px] rounded-[10px]">
-              <img src={profile} className="w-[24px] h-[24px] rounded-full" />
-              <figcaption>imaria0218</figcaption>
-            </figure>
-            {/* 임시 룸 상태 이동 버튼 */}
-            <button
-              onClick={() => {
-                setIsWaiting(false);
-              }}
-              className="text-white font-bold"
-            >
-              대기 완료
-            </button>
-          </div>
+          <AudienceListBox setIsWaiting={setIsWaiting} />
+          {/* TODO: 대기 완료 전환 버튼은 임시로 제작해 놓은 것으로 실제 구현시 props 및 버튼을 삭제하십시오 */}
         </div>
         <ObserverMatchingInterface isWaiting={isWaiting} />
       </section>
@@ -50,7 +35,7 @@ export default function ObserverWaitingRoom() {
           </p>
         </div>
         {/* 최대 */}
-        <div className=" w-full font-jersey text-white flex flex-col items-end gap-[5px]">
+        <div className="md:block hidden w-full font-jersey text-white flex flex-col items-end gap-[5px]">
           <div className="w-[188px] flex justify-start">
             <p>audience</p>
           </div>
