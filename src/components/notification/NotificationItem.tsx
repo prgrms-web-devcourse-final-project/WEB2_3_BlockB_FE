7b21a4acc45e9c01905da422d145055ddadc4d6c
Kim, DongChan <kdc9050@naver.com>
2025-02-25 01:11:11 +0900

    Feat: 알림 반응형 및 Header 고정

--- src/components/notification/NotificationItem.tsx
+++ src/components/notification/NotificationItem.tsx
@@ -11,18 +11,19 @@ export default function NotificationItem({
 }) {
   return (
     <div
-      className={`flex justify-between items-center px-3 py-2 rounded-md transition duration-200 ${
-        isDisabled ? "opacity-50" : "text-gray-700 hover:bg-gray-100"
-      }`}
+      className={`flex justify-between items-center  py-2 rounded-md transition duration-200 
+        ${isDisabled ? "opacity-50" : "text-gray-700 hover:bg-gray-200"}
+      `}
     >
       <div className="flex items-center space-x-2">
         <div
-          className={`w-2 h-2 rounded-full ${
+          className={`w-2 h-2 m-1 rounded-full flex-shrink-0 ${
             isNew ? "bg-game_blue01" : "bg-gray-400"
           }`}
         ></div>
+
         <p
-          className={`text-sm transition duration-200 ${
+          className={`text-xs sm:text-sm transition duration-200 ${
             isNew ? "text-gray-700" : "text-gray-400"
           }`}
         >
@@ -31,9 +32,9 @@ export default function NotificationItem({
       </div>
       {actionText && (
         <button
-          className={`text-sm transition duration-200 ${
+          className={`text-xs sm:text-sm mr-1 transition duration-200 whitespace-nowrap ${
             isNew
-              ? "text-game_blue01 hover:underline hover:text-game_blue02"
+              ? "text-game_blue01 hover:underline hover:text-blue-700"
               : "text-gray-400"
           }`}
           disabled={isDisabled}
