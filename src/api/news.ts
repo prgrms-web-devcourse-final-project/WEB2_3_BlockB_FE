854347e7989a5651d4db9e273f20e3516f986ade
naehyun <scott253@naver.com>
2025-02-26 16:50:10 +0900

    feat:news페이지 api 연동 및 최신순/인기순 정렬

--- src/api/news.ts
+++ src/api/news.ts
@@ -1,8 +1,19 @@
 import { axiosInstance } from "./axios";
 
+const getAllNews = async (sort: string) => {
+  try {
+    const response = await axiosInstance.get("/api/news", {
+      params: { sort },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 const getNewsTop10 = async () => {
   try {
-    const response = await axiosInstance.get(`/api/news/ranking`);
+    const response = await axiosInstance.get("/api/news/ranking");
     return response.data;
   } catch (error) {
     throw error;
@@ -10,5 +21,6 @@ const getNewsTop10 = async () => {
 };
 
 export const newsAPI = {
+  getAllNews,
   getNewsTop10,
 };
