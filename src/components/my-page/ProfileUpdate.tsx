54790ad7f9a4ac7f7e88946bb28647a9b596445a
engsal-bin <imaria0218@gmail.com>
2025-02-27 15:50:14 +0900

    feat: 프로필 수정 구현(사진 제외)

--- src/components/my-page/ProfileUpdate.tsx
+++ src/components/my-page/ProfileUpdate.tsx
@@ -1,15 +1,34 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import avatar from "../../assets/icons/avatar.svg";
+import { useNavigate } from "react-router-dom";
 import edit from "../../assets/icons/edit.svg";
 import ProfileUpdateSkeleton from "../common/skeleton/mypage/ProfileUpdateSkeleton";
+import { userApi } from "../../api/user";
 
 export default function ProfileUpdate() {
   const [isLoading, setIsLoading] = useState(true);
+  const [newNickname, setNewNickname] = useState("")
+  const [userId, setUserId] = useState<number | null>(null)
+  const [newIntroduction, setNewIntroduction] = useState("")
+  const [newProfileImg, setNewProfileImg] = useState("")
 
-  useEffect(() => {
-    setTimeout(() => setIsLoading(false), 1000);
-  }, []);
+  useEffect(()=>{
+    const loadOriginalProfile = async() => {
+      const profileResponse = await userApi.fetchMyProfile()
+      setUserId(profileResponse.data.id)
+      setNewNickname(profileResponse.data.nickname)
+      setNewIntroduction(profileResponse.data.introduction || "")
+      setNewProfileImg(profileResponse.data.profileUrl)
+      setIsLoading(false)
+    }
+    loadOriginalProfile()
+},[])
+
+  const navigate = useNavigate()
+    
+  const onSubmitUpdatedProfile = async() => {
+    await userApi.updateUserProfile(userId!, {nickname: newNickname, introduction: newIntroduction, profileUrl: newProfileImg})
+    navigate("/my-page")
+  }
 
   if (isLoading) return <ProfileUpdateSkeleton />;
 
@@ -19,7 +38,7 @@ export default function ProfileUpdate() {
         <div className="md:max-w-[500px] max-w-[366px] h-[500px]  flex flex-col justify-between">
           <div className="flex justify-center">
             <img
-              src={avatar}
+              src={newProfileImg}
               alt="프로필 이미지"
               className="w-[200px] h-[200px] rounded-full max-md:w-36 max-md:h-36"
             />
@@ -34,33 +53,37 @@ export default function ProfileUpdate() {
               />
             </button>
           </div>
-          {["닉네임 변경", "자기소개 변경"].map((title, index) => {
-            const placeholder = [
-              "변경하실 닉네임을 입력해주세요.",
-              "변경하실 자기소개를 입력해주세요.",
-            ];
-            return (
-              <div
-                className="flex md:items-center items-start justify-between max-md:flex-col"
-                key={index}
-              >
-                <span className="md:text-[18px] text-[16px] md:mr-3 mb-1">{title}</span>
-                <input
-                  type="text"
-                  placeholder={placeholder[index]}
-                  className="w-[366px] h-12 bg-gray02 rounded-lg pl-5 max-md:w-[300px]"
-                />
-              </div>
-            );
-          })}
-
+          <div
+            className="flex md:items-center items-start justify-between max-md:flex-col"
+          >
+            <span className="md:text-[18px] text-[16px] md:mr-3 mb-1">닉네임 변경</span>
+            <input
+              type="text"
+              value={newNickname}
+              placeholder="변경하실 닉네임을 입력해주세요."
+              onChange={(e)=> setNewNickname(e.target.value)}
+              className="w-[366px] h-12 bg-gray02 rounded-lg pl-5 max-md:w-[300px]"
+            />
+          </div>
+          <div
+            className="flex md:items-center items-start justify-between max-md:flex-col"
+          >
+            <span className="md:text-[18px] text-[16px] md:mr-3 mb-1">자기소개 변경</span>
+            <input
+              type="text"
+              value={newIntroduction}
+              placeholder="변경하실 소개글을 입력해주세요."
+              onChange={(e) => setNewIntroduction(e.target.value)}
+              className="w-[366px] h-12 bg-gray02 rounded-lg pl-5 max-md:w-[300px]"
+            />
+          </div>
           <div className="flex justify-end h-[60px] ">
-            <Link
-              to={"/my-page"}
+            <button
+              onClick={onSubmitUpdatedProfile}
               className="flex bg-blue03 w-[84px] h-10 rounded-[10px] md:text-[16px] text-[14px] text-white justify-center items-center"
             >
               저장
-            </Link>
+            </button>
           </div>
         </div>
       </div>
