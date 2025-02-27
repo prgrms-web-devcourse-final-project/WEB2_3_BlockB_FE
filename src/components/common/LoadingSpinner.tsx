b4643f6ece00dfaa5a79db1d82876f7ddcc089cd
engsal-bin <imaria0218@gmail.com>
2025-02-27 10:18:59 +0900

    feat: loadingSpinner optional props 추가

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,4 +1,5 @@
-export default function LoadingSpinner ()  {
+// TODO: option string 타입으로 임시 지정. 추후에 boolean 값으로 조건 넣고 싶다면 수정하십시오.
+export default function LoadingSpinner ({option}: {option?: string})  {
     return (
       <div className="flex items-center justify-center h-screen">
         <div className="w-10 h-10 border-4 border-gray03 border-t-game_blue01 rounded-full animate-spin"></div>
