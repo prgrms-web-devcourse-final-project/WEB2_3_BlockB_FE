a5bad0877c258f2b48202f9da61a13875761c196
Kim, DongChan <kdc9050@naver.com>
2025-02-27 10:35:45 +0900

    Feat: 로그인 전 후 라우팅 수정

--- src/pages/OAuthCallback.tsx
+++ src/pages/OAuthCallback.tsx
@@ -5,21 +5,24 @@ import { useAuthStore } from "../stores/authStore";
 export default function OAuthCallback() {
   const location = useLocation();
   const navigate = useNavigate();
-  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  const { setAccessToken, setRefreshToken } = useAuthStore();
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     const accessToken = params.get("access_token");
+    const refreshToken = params.get("refresh_token");
 
-    if (accessToken) {
+    if (accessToken && refreshToken) {
       setAccessToken(accessToken);
+      setRefreshToken(refreshToken);
       console.log("로그인 성공");
+      console.log("Access Token:", accessToken);
+      console.log("Refresh Token:", refreshToken);
       navigate("/signup", { replace: true });
     } else {
       console.error("Access token이 존재하지 않습니다.");
       navigate("/login", { replace: true });
     }
   }, [location, navigate, setAccessToken]);
-
   return <div>로그인중</div>;
 }
