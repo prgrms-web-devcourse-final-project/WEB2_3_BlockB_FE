e05e14103fbca4200a54a8d454d39f912a7fc75c
engsal-bin <imaria0218@gmail.com>
2025-02-27 09:23:19 +0900

    design: admin footer간 사이 조정

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -101,7 +101,7 @@ export default function Admin() {
 
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center mb-10">
         <div className="w-full max-w-[980px] h-auto mt-[50px] pt-7 pl-7 pr-7">
           <p className="text-[24px] font-bold md:text-[32px]">신고 목록</p>
           <AdminTab tab={tab} setTab={setTab}/>
