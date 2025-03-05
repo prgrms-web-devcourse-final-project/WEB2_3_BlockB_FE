03373b2c7418b3236da7d591cf6ec8e0a8e8c8e5
Kim, DongChan <kdc9050@naver.com>
2025-03-05 16:10:16 +0900

    Fix: 퍼블릭 라우팅 수정

--- src/layouts/PublicRoute.tsx
+++ src/layouts/PublicRoute.tsx
@@ -1,9 +1,20 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../stores/authStore";
+import { useUserStore } from "../stores/userStore";
+
 const PublicRoute = () => {
   const accessToken = useAuthStore((state) => state.accessToken);
+  const isNewUser = useAuthStore((state) => state.isNewUser);
+  const userId = useUserStore((state) => state.userId);
 
-  return accessToken ? <Navigate to="/main" replace /> : <Outlet />;
+  if (accessToken) {
+    if (isNewUser && userId) {
+      return <Navigate to={`/signup/${userId}`} replace />;
+    }
+    return <Navigate to="/main" replace />;
+  }
+
+  return <Outlet />;
 };
 
 export default PublicRoute;
