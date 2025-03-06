6509295f072994864c7a6ef0af7203a2bd0d86b8
Kim, DongChan <kdc9050@naver.com>
2025-03-06 22:32:42 +0900

    Fix: 타임 스탬프 수정

--- src/components/user-page/CommonSimpleInfo.tsx
+++ src/components/user-page/CommonSimpleInfo.tsx
@@ -13,7 +13,9 @@ export default function CommonSimpleInfo({
       <div className="flex text-[16px] md:items-center max-md:flex-col">
         <div className="w-[100px] h-7 mr-[18px] bg-blue01 text-white flex items-center justify-center rounded-[10px]">
           <span className="mr-1">
-            {"continent" in data ? continentKR[data.continent] : categoryKR[data.category]}
+            {"continent" in data
+              ? continentKR[data.continent]
+              : categoryKR[data.category]}
           </span>
           {type === "debate" && <img src={politics} alt="정치 아이콘" />}
         </div>
@@ -21,7 +23,9 @@ export default function CommonSimpleInfo({
       </div>
       {type === "news" ? (
         <div className="text-[18px] text-gray04 font-semibold">
-          {(data as MyNews).createdAt}
+          {new Date((data as MyNews).createdAt).toLocaleString("ko-KR", {
+            timeZone: "Asia/Seoul",
+          })}
         </div>
       ) : (
         <div className="text-[18px] text-gray04 font-semibold flex">
