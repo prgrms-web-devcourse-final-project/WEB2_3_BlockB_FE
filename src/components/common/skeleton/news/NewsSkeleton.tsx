bf1ac0f201cc5f6c610fff4846cbf0e8eacdcee5
Kim, DongChan <kdc9050@naver.com>
2025-03-06 23:42:27 +0900

    Fix: 뉴스스켈레톤 수정

--- src/components/common/skeleton/news/NewsSkeleton.tsx
+++ src/components/common/skeleton/news/NewsSkeleton.tsx
@@ -6,11 +6,9 @@ export default function NewsSkeleton() {
           <div className="w-full h-48 bg-gray03 rounded-lg"></div>
           <div className="mt-3 h-6 bg-gray03 rounded w-3/4"></div>
           <div className="mt-2 h-4 bg-gray03 rounded w-1/2"></div>
-          <div className="mt-2 h-4 bg-gray03 rounded w-full"></div>
+          <div className="mt-2 h-6 bg-gray03 rounded w-full"></div>
           <div className="mt-3 flex justify-end items-center text-gray-500 text-sm">
             <div className="flex space-x-3">
-              <div className="w-5 h-5 bg-gray03 rounded-full"></div>
-              <span className="w-6 h-4 bg-gray03 rounded"></span>
               <div className="w-5 h-5 bg-gray03 rounded-full"></div>
               <span className="w-6 h-4 bg-gray03 rounded"></span>
               <div className="w-5 h-5 bg-gray03 rounded-full"></div>
