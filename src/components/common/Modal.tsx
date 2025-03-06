b625809b57ee41b8ff419aa964c7e3952377f2e2
Kim, DongChan <kdc9050@naver.com>
2025-03-07 01:34:20 +0900

    Fix: 모달 콘솔 제거

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,25 +1,21 @@
 import RoomActionButtons from "../debate-zone/RoomActionButtons";
 import { useModalStore } from "../../stores/useModal";
-import { useEffect } from "react";
 
 export default function Modal() {
   const { isOpen, message, onConfirm, closeModal, btnType } = useModalStore();
-  
-  useEffect(()=> {
-    console.log("message:", JSON.stringify(message));
-  },[isOpen, message])
-  
 
   if (!isOpen) return null;
   return (
     <div className="fixed inset-0 bg-black01 z-50 bg-opacity-70 flex flex-col justify-center items-center font-pretendard">
       <div className="bg-white flex flex-col h-auto w-[298.6px] gap-[10px] justify-center px-[50px] py-[30px] rounded-[10px] whitespace-pre-line">
-        <p className="text-black01 font-bold w-full break-words whitespace-pre-line text-center">{message}</p>
+        <p className="text-black01 font-bold w-full break-words whitespace-pre-line text-center">
+          {message}
+        </p>
         <div className="flex justify-center">
           <RoomActionButtons
             cancelAction={closeModal}
             confirmAction={() => {
-              onConfirm()
+              onConfirm();
               closeModal();
             }}
             cancelColor="bg-gray03 text-white"
@@ -32,4 +28,3 @@ export default function Modal() {
     </div>
   );
 }
-
