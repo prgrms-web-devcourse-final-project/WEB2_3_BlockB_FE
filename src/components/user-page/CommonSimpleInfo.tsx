6c4398ed07ed828f0fc7461f68f2f604fba9ebc8
engsal-bin <imaria0218@gmail.com>
2025-03-09 00:39:14 +0900

    feat: 저장된 뉴스 뉴스 페이지로 다이렉팅

--- src/components/user-page/CommonSimpleInfo.tsx
+++ src/components/user-page/CommonSimpleInfo.tsx
@@ -1,15 +1,23 @@
+import { useNavigate } from "react-router";
 import politics from "../../assets/icons/politics.svg";
 import { categoryKR, continentKR } from "../../constants";
 
-export default function CommonSimpleInfo({
-  type,
-  data,
-}: {
-  type: string;
-  data: MyNews | ArchivedDebate;
-}) {
+type CommonSimpleInfoProps =
+  | { type: "news"; data: MyNews }
+  | { type: "debate"; data: ArchivedDebate };
+
+export default function CommonSimpleInfo({ type, data }: CommonSimpleInfoProps) {
+  const navigate = useNavigate();
+
+  const goToLinkedPage = () => {
+    type === "news" ? navigate(`/news/${data.newsId}`) : navigate(`/`); // TODO: 뉴스 아카이빙 페이지 추가 예정
+  };
+
   return (
-    <div className="flex justify-between w-full py-4 px-2 max-md:flex-col rounded-[10px] hover:bg-gray02 transform transition duration-200 hover:scale-[1.02]">
+    <div
+      onClick={goToLinkedPage}
+      className="flex justify-between w-full py-4 px-2 max-md:flex-col rounded-[10px] hover:bg-gray02 transform transition duration-200 hover:scale-[1.02]"
+    >
       <div className="flex text-[16px] md:items-center max-md:flex-col">
         <div className="w-[100px] h-7 mr-[18px] bg-blue01 text-white flex items-center justify-center rounded-[10px]">
           <span className="mr-1">
@@ -21,17 +29,18 @@ export default function CommonSimpleInfo({
         </div>
         <div className="font-bold">{data.title}</div>
       </div>
+
       {type === "news" ? (
         <div className="text-[18px] text-gray04 font-semibold">
-          {new Date((data as MyNews).createdAt).toLocaleString("ko-KR", {
+          {new Date(data.createdAt).toLocaleString("ko-KR", {
             timeZone: "Asia/Seoul",
           })}
         </div>
       ) : (
         <div className="text-[18px] text-gray04 font-semibold flex">
-          <div>{(data as ArchivedDebate).time}</div>
-          <div className="mx-2">{(data as ArchivedDebate).member}</div>
-          <div>{(data as ArchivedDebate).isParticipant ? "참여" : "참관"}</div>
+          <div>{data.time}</div>
+          <div className="mx-2">{data.member}</div>
+          <div>{data.isParticipant ? "참여" : "참관"}</div>
         </div>
       )}
     </div>
