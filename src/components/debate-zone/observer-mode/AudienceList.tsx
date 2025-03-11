7e53881e63bb8cc168d6dc20b7afa792af32cb2a
engsal-bin <imaria0218@gmail.com>
2025-03-11 17:29:15 +0900

    feat: 신고 모달 audiencelist 제거 대신 pro con 참관자 md 이상 화면에서도 볼 수 있도록 수정

--- src/components/debate-zone/observer-mode/AudienceList.tsx
+++ src/components/debate-zone/observer-mode/AudienceList.tsx
@@ -1,35 +1,72 @@
+import { useParams } from "react-router";
 import flag from "../../../assets/icons/flag-white.svg";
 import profile from "../../../assets/icons/profile-white.svg";
 import { useReportModalStore } from "../../../stores/reportModalStore";
+import { useEffect } from "react";
+import { debateRoomApi } from "../../../api/debatezone";
+import { useDebateWebSocket } from "../../../contexts/DebateWebSocketContext";
 
 export default function AudienceList() {
   const { openModal } = useReportModalStore();
 
-  const handleOpenReportModal = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.stopPropagation()
+  const handleOpenReportModal = (userNickname: string, userId: number) => (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
     openModal({
-      targetNickname: "김예빈 어드민 계정",
-      targetUserId: 2,
+      targetNickname: userNickname,
+      targetUserId: userId,
       targetType: "CHAT",
       roomId: null,
     });
   };
 
+  const { roomId } = useParams();
+  const { setRoomInfoDetails, roomInfoDetails } = useDebateWebSocket();
+
+  useEffect(() => {
+    const fetchRoomInfoInObserverZone = async () => {
+      if (!roomId) return;
+      const { data } = await debateRoomApi.fetchObserverOngoingRoomInfo(roomId);
+      setRoomInfoDetails(data);
+    };
+    fetchRoomInfoInObserverZone();
+  }, [roomId]);
+
   return (
     <div className="hidden md:flex w-full h-[176px] font-jersey text-white flex-col justify-normal items-end gap-[5px]">
       <div className="w-[188px] flex justify-start">
-        <p>audience</p>
+        <p>participants</p>
       </div>
       <div className="flex flex-col gap-[5px] overflow-y-auto">
-        <figure className="flex justify-between px-[5px] py-[2px] items-center bg-white bg-opacity-40 w-[188px] h-[33px] rounded-[10px]">
-          <div className="flex gap-[11px]">
-            <img src={profile} className="w-[24px] h-[24px] rounded-full" />
-            <figcaption>imaria0218</figcaption>
-          </div>
-          <button onClick={handleOpenReportModal} className="mr-[10px]">
-            <img src={flag} alt="신고하기 버튼" />
-          </button>
-        </figure>
+        <p className="font-jersey">pro</p>
+        {roomInfoDetails.proUsers.map((user) => (
+          <figure
+            key={user.id}
+            className="flex justify-between px-[5px] py-[2px] items-center bg-white bg-opacity-40 w-[188px] h-[33px] rounded-[10px]"
+          >
+            <div className="flex gap-[11px]">
+              <img src={user.profileUrl || profile} className="w-[24px] h-[24px] rounded-full" />
+              <figcaption>{user.nickname}</figcaption>
+            </div>
+            <button onClick={handleOpenReportModal(user.nickname, user.id)} className="mr-[10px]">
+              <img src={flag} alt="신고하기 버튼" />
+            </button>
+          </figure>
+        ))}
+        <p className="font-jersey">con</p>
+        {roomInfoDetails.conUsers.map((user) => (
+          <figure
+            key={user.id}
+            className="flex justify-between px-[5px] py-[2px] items-center bg-white bg-opacity-40 w-[188px] h-[33px] rounded-[10px]"
+          >
+            <div className="flex gap-[11px]">
+              <img src={user.profileUrl || profile} className="w-[24px] h-[24px] rounded-full" />
+              <figcaption>{user.nickname}</figcaption>
+            </div>
+            <button onClick={handleOpenReportModal(user.nickname, user.id)} className="mr-[10px]">
+              <img src={flag} alt="신고하기 버튼" />
+            </button>
+          </figure>
+        ))}
       </div>
     </div>
   );
