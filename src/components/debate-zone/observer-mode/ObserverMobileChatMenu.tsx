ae5d52cff487274fa93ca2aac119023ba07cfa86
engsal-bin <imaria0218@gmail.com>
2025-03-12 00:58:37 +0900

    fix: messageItem map 시 고유값 누락 문제 해결

--- src/components/debate-zone/observer-mode/ObserverMobileChatMenu.tsx
+++ src/components/debate-zone/observer-mode/ObserverMobileChatMenu.tsx
@@ -2,8 +2,6 @@ import { useEffect, useRef, useState } from "react";
 
 import hambuger from "../../../assets/icons/hamburger.svg"
 import ParticipantBox from "../ParticipantBox";
-import AudienceCard from "../AudienceCard";
-import profile from "../../../assets/icons/profile-white.svg"
 import exit from "../../../assets/icons/exit.svg"
 import Counter from "../ongoing-debate/Counter";
 import ExitModal from "../../common/Modal";
@@ -56,8 +54,7 @@ export default function ObserverMobileChatMenu() {
         {/* 나가기 모달 */}
         <ExitModal />
         <div className="flex justify-between text-white font-jersey flex sm:gap-[60px] gap-[40px]">
-            <Counter label="TURN" boxNumber={2} />
-            <Counter label="TIMER" boxNumber={3} />
+            <Counter label="TURN" boxNumber={2} isObserverRoom={true}/>
         </div>
         <button onClick={()=>setIsSideBarOpen(!isSidebarOpen)}>
             <img src={hambuger} alt="사이드 바 버튼" />
@@ -65,23 +62,14 @@ export default function ObserverMobileChatMenu() {
         {/* 사이드바 */}
         {isSidebarOpen && <section ref={sidebarRef} className={`absolute right-0 top-0 min-h-screen bg-black w-1/2 flex flex-col gap-4 p-6 
         ${isSidebarOpen ? "animate-slideIn" : "animate-slideOut"}`}>
-            <div className="flex justify-end">
-                <button onClick={handleExitClick}>
-                    <img src={exit} alt="나가기 버튼" />
-                </button>
-            </div>
-            <ParticipantBox label="PROS" labelAlignment = "left" hasReportBtn ={true} participants={roomInfoDetails?.proUsers}/>
-            <ParticipantBox label="CONS" labelAlignment = "left" hasReportBtn ={true} participants={roomInfoDetails?.conUsers}/>
-            <div className="flex flex-col gap-4 text-white">
-                {/* TODO: 삭제해야 함 */}
-                <p className="font-jersey text-[16px]">Audience</p>
-                <div className="flex flex-col gap-1 pl-2">
-                    <AudienceCard profile={profile} nickname="imaria0218"/>
-                    <AudienceCard profile={profile} nickname="imaria0218"/>
-                    <AudienceCard profile={profile} nickname="imaria0218"/>
-                    <AudienceCard profile={profile} nickname="imaria0218"/>
-                    <AudienceCard profile={profile} nickname="imaria0218"/>
+                <div className="flex justify-end">
+                    <button onClick={handleExitClick}>
+                        <img src={exit} alt="나가기 버튼" />
+                    </button>
                 </div>
+                <ParticipantBox label="PROS" labelAlignment = "left" hasReportBtn ={true} participants={roomInfoDetails?.proUsers}/>
+                <ParticipantBox label="CONS" labelAlignment = "left" hasReportBtn ={true} participants={roomInfoDetails?.conUsers}/>
+                <div className="flex flex-col gap-4 text-white">
              </div>
         </section>}
     </div>
