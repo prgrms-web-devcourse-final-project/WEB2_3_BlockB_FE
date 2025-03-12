d2379648c91baded380a4019ed948f83f7808320
engsal-bin <imaria0218@gmail.com>
2025-03-12 10:48:21 +0900

    feat: 웹소켓에 투표 타이머 기능 추가 (+아카이빙 css 수정)

--- src/components/archived-room/ArchivedRoomObserverChat.tsx
+++ src/components/archived-room/ArchivedRoomObserverChat.tsx
@@ -15,20 +15,20 @@ export default function ArchivedRoomObserverChat({ isDebateTabed, logs }: { isDe
   return (
         <section
           id="chatwindow"
-          className={`md:w-full md:h-[440px] max-h-[calc(100vh-90px)] rounded-[10px] md:mt-0 mt-[10px] flex flex-col flex-grow pb-2
+          className={`md:w-full md:h-[440px] max-h-[calc(100vh-60px)] rounded-[10px] md:mt-0 mt-[10px] flex flex-col flex-grow pb-2
             ${isDebateTabed ? "hidden md:flex" : "h-screen"}`}
         >
           {/* 메시지 로그 */}
           <div className="flex-grow overflow-y-auto">
-          <div className="md:w-[378px] md:h-[376px] flex flex-col flex-grow p-[10px] font-pretendard flex flex-col gap-[12px] overflow-y-auto">
+          <div className="md:w-[378px] md:h-[376px] flex flex-col gap-[10px] flex-grow p-[10px] font-pretendard flex flex-col gap-[12px] overflow-y-auto">
 
-            {logs.map((msg)=>
-            <ChatObserverChatBubble
-                isMine={msg.nickname === currentUserName}
-                username={msg.nickname}
-                message={msg.content}
-                profileUrl={msg.profileUrl}
-            />)}
+              {logs.map((msg)=>
+              <ChatObserverChatBubble
+                  isMine={msg.nickname === currentUserName}
+                  username={msg.nickname}
+                  message={msg.content}
+                  profileUrl={msg.profileUrl}
+              />)}
 
             </div>
           </div>
