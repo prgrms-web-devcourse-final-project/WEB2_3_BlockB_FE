9f3a0a6e4492e3bd965bfd03379d3888c90d19e7
naehyun <scott253@naver.com>
2025-03-12 20:36:31 +0900

    syle: 알림 없음 스타일 변경

--- src/components/notification/NotificationList.tsx
+++ src/components/notification/NotificationList.tsx
@@ -8,6 +8,8 @@ import { useUserStore } from "../../stores/userStore";
 import useGetNotifications from "../../hooks/useGetNotifications";
 import { useInView } from "react-intersection-observer";
 import { useQueryClient } from "@tanstack/react-query";
+import noNotification from "../../assets/icons/no-notification.svg";
+import NotificationDeleteModal from "./NotificationDeleteModal";
 
 export default function NotificationList({
   status,
@@ -116,27 +118,28 @@ export default function NotificationList({
             >
               ✕
             </button>
-            {localStorage.getItem("fcmToken") && (
-              <div className="flex gap-2">
-                {[
-                  {
-                    content: "모두 삭제",
-                    function: () => {
-                      setDeleteModal("allDelete");
+            {localStorage.getItem("fcmToken") &&
+              data!.pages[0].data.notifications.content.length > 0 && (
+                <div className="flex gap-2">
+                  {[
+                    {
+                      content: "모두 삭제",
+                      function: () => {
+                        setDeleteModal("allDelete");
+                      },
                     },
-                  },
-                  { content: "모두 읽기", function: allNotificationsRead },
-                ].map((item, index) => (
-                  <button
-                    onClick={item.function}
-                    key={index}
-                    className="mt-3 mb-3 text-xs text-gray-500 sm:text-sm hover:text-gray-700"
-                  >
-                    {item.content}
-                  </button>
-                ))}
-              </div>
-            )}
+                    { content: "모두 읽기", function: allNotificationsRead },
+                  ].map((item, index) => (
+                    <button
+                      onClick={item.function}
+                      key={index}
+                      className="mt-3 mb-3 text-xs text-gray-500 sm:text-sm hover:text-gray-700"
+                    >
+                      {item.content}
+                    </button>
+                  ))}
+                </div>
+              )}
           </div>
         </div>
 
@@ -162,7 +165,12 @@ export default function NotificationList({
                 <div ref={ref} className="w-full h-10 "></div>
               </>
             ) : (
-              <p className="w-80">알림 없음</p>
+              <div className="flex flex-col items-center justify-center w-full h-48 gap-3">
+                <img src={noNotification} alt="알림 없음 아이콘" />
+                <p className="font-semibold font-sofiaSans text-[14px]">
+                  알림이 없습니다.
+                </p>
+              </div>
             )
           ) : (
             <div className="flex flex-col gap-2 mt-4">
@@ -197,39 +205,13 @@ export default function NotificationList({
         </div>
       </div>
       {deleteModal && (
-        <div
-          className={`absolute w-[420px] top-[50px] h-72 overflow-auto md:top-[85px] right-[15px] md:right-[30px] mx-auto p-3 sm:p-4 rounded-lg shadow-lg max-w-[90%] sm:max-w-[400px]  lg:max-w-[420px] border border-gray-200 bg-black/40 `}
-        >
-          <div className="flex items-center w-full h-full">
-            <div className="w-full p-4 rounded-lg bg-white/80 backdrop-blur-md">
-              <p className="mb-4 font-semibold text-center text-gray-800">
-                정말 삭제하시겠습니까?
-              </p>
-              <div className="flex justify-center gap-4">
-                <button
-                  className="px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600"
-                  onClick={() => {
-                    if (deleteModal === "delete") {
-                      notificationDelete(deleteNotificationId);
-                    }
-                    if (deleteModal === "allDelete") {
-                      allNotificationsDelete();
-                    }
-                    closeDeleteModal();
-                  }}
-                >
-                  삭제
-                </button>
-                <button
-                  onClick={closeDeleteModal}
-                  className="px-4 py-2 text-gray-800 bg-gray-300 rounded-lg hover:bg-gray-400"
-                >
-                  취소
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <NotificationDeleteModal
+          deleteModal={deleteModal}
+          notificationDelete={notificationDelete}
+          deleteNotificationId={deleteNotificationId}
+          allNotificationsDelete={allNotificationsDelete}
+          closeDeleteModal={closeDeleteModal}
+        />
       )}
     </div>
   );
